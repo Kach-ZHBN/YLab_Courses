3d7c3b473943ef942d017f378023487e534dda2d
Kach ZHBN <kach.zhbn@gmail.com>
2023-03-12 09:59:20 +0300

    add task1 sequences

---
+++ src/Lesson_2/sequences/SequencesTest.java
@@ -0,0 +1,18 @@
+package Lesson_2.sequences;
+
+public class SequencesTest {
+    public static void main(String[] args) {
+        int N = 15;
+        Sequences sequences = new SequencesImpl();
+        sequences.a(N);
+        sequences.b(N);
+        sequences.c(N);
+        sequences.d(N);
+        sequences.e(N);
+        sequences.f(N);
+        sequences.g(N);
+        sequences.h(N);
+        sequences.i(N);
+        sequences.j(N);
+    }
+}
